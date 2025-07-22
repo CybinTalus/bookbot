e1fc2abe67643b7f875ebd86b03c57ea933e5000
CybinTalus <CybinTalus@proton.me>
2025-07-21 20:34:52 -0400

    Updated main.py to use CLI path to book

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import word_counter, letter_counter, sorter
+import sys
 
-filepath = "books/frankenstein.txt"
+# filepath = "books/frankenstein.txt"
 
 def get_book_text(file):
     with open(file) as f:
@@ -13,6 +14,10 @@ def main():
     # get_book_text(filepath)
     # word_counter(filepath)
     # letter_counter(filepath)
-    sorter(filepath)
-    
+    # sorter(filepath)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    sorter(sys.argv[1])
+        
 main()
